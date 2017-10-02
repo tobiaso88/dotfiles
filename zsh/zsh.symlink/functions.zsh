5b8c9e5f61a475f90ae54253179dee44d13c4751
Tobias Olsson <firewolf882002@hotmail.com>
2017-10-02 08:52:05 +0200

    Add new functions

--- zsh/zsh.symlink/functions.zsh
+++ zsh/zsh.symlink/functions.zsh
@@ -1,3 +1,16 @@
 function add_to_path () {
     export PATH="$1:$PATH"
 }
+
+function site () {
+    cd ~/Sites/$1
+}
+
+function nxt () {
+    site nxt/$1
+}
+
+function 52 () {
+    site 52/$1
+}
+

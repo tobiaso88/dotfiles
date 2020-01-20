cbc30fe64aa079011920857082bdffb12ccae14d
Tobias Olsson <info@tobias-olsson.se>
2020-01-20 08:21:59 +0100

    Move config to config file

--- zsh/zsh.symlink/config.zsh
+++ zsh/zsh.symlink/config.zsh
@@ -8,3 +8,6 @@ export GREP_OPTIONS='--color=auto'
 export VISUAL=vim
 export EDITOR=$VISUAL
 export HOMEBREW_EDITOR=/usr/local/bin/subl
+
+export EXAKTAHOSTING_USERNAME="wh@exaktasoftware.se"
+export EXAKTAHOSTING_PASSWORD="123123"

ba2051bef61b87628ef2eccf9d26a02dc4045c37
Tobias Olsson <info@tobias-olsson.se>
2020-01-20 08:26:38 +0100

    Update aliases

--- zsh/zsh.symlink/alias.zsh
+++ zsh/zsh.symlink/alias.zsh
@@ -4,38 +4,25 @@ alias aliases='subl ~/.zsh/alias.zsh'
 # Edit the hosts file
 alias hosts='subl /etc/hosts'
 
-# Open the virtualhosts folder in sublime
-alias virtualhosts='subl ~/.virtualhosts'
-
 # Edit the hosts file
 alias httpde='subl /usr/local/etc/httpd/httpd.conf'
 
 # Edit the .zshrc file
 alias zs='subl ~/.zshrc'
-alias csd='cap staging deploy'
-alias cpd='cap production deploy'
 
 ## Git stuff
 alias gmc='git fetch core && git merge --no-ff core/develop'
-alias rebasedev="git fetch --all && git rebase origin/develop"
+alias bumppatch='bumpversion --allow-dirty patch && ga .bumpversion.cfg VERSION && git commit -m "Bump version number"'
 
 # Mysql
 alias mysqlstart='sudo /usr/local/mysql/support-files/mysql.server start'
 alias mysqlrestart='sudo /usr/local/mysql/support-files/mysql.server restart'
 alias mysqlstop='sudo /usr/local/mysql/support-files/mysql.server stop'
 
-alias vh='virtualhost.sh'
-
 # Sass watcher
 alias swatch='sass --watch . --style expanded'
 
 alias dockercreatemysql='docker run -i -t -p 3306:3306 -v ${PWD}/mysql:/var/lib/mysql -v ${PWD}/mysql.conf.d:/etc/mysql/conf.d -e MYSQL_ALLOW_EMPTY_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --name db mysql'
-
-# Customer ssh
-alias jonic='ssh jonictextil@server6.c4hosting.se'
-alias jackan='ssh root@jackan.com'
-
-alias phpv='brew-php-switcher'
 
 # Lock the screen (when going AFK)
 alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
@@ -50,3 +37,5 @@ alias sites="cd ~/Sites"
 
 # Desk
 alias d.="desk go"
+
+al(){alias | grep ^$@}
